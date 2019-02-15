a58fa3a6e1add04f1760a5c5db40fb20e16c0aee
AlexTurchyn <oleksii.turchyn@nure.ua>
2019-02-15 21:53:42 +0200

    Task5B

--- Hometask4/index.js
+++ Hometask4/index.js
@@ -8,107 +8,136 @@ var arrPizzas = [{name:"4 сезони кохання",filling:"м’ясо ка
 {name:"Чотири сири",filling:"Пармезан, дор блю, чеддер, моцарела, груша, волоський горіх, соус вершковий",calories:1500, cost:144,imageURL:"./images/ekstravagantnaja.jpg"},{name:"Маргарита",filling:"Томати, моцарела, орегано, базилік, соус Pomodoro",calories:3000, cost:290,imageURL:"./images/gavajskaja.jpg"}];
 
 
-function noImage() {
-    if(!("imageURL" in this) || this.imageURL.length == 0) return true;
-    return false;
-}
-function createFillingUl() {
-    let ul = document.createElement('ul');
-    let arr = this.filling.split(",");
-    arr.forEach(item =>{
-        let li = document.createElement('li');
-        li.innerHTML = item;
-        ul.appendChild(li);
-    });
-    return ul.innerHTML;
-}
-function createPizzaGrid(pizza) {
-    let div = document.createElement('div');
-    let li  = document.createElement('li');
-    let name = document.createElement('p')
-    let img = document.createElement('img');
-    let label = document.createElement('label');
-    let str = document.createElement("div")
-
-    if(noImage.call(pizza)) {
-        console.log("There is no photo for pizza " + name)
-        img.src = "./images/default.jpg";
-    } else {
-        img.src = pizza.imageURL;
+class Pizza {
+    constructor (pizza) {
+        this.imageURL = pizza.imageURL,
+        this.name = pizza.name,
+        this.filling = pizza.filling;
+        this.calories =pizza.calories;
+        this.cost = pizza.cost;
+        this.matchesTheFilter = true;
     }
-    // img.classList.add('hidden');
-    name.innerHTML = pizza.name.toUpperCase();
-    name.className = "name";
-    label.innerHTML = "Состав";
-    label.htmlFor = `press${pizza.name}`;
-    str.className="spoiler";
-    str.innerHTML = `
-        <input type="checkbox" id="press${pizza.name}" class="used">
-        <div class="block">
-            <ul class = "filling">
-                ${createFillingUl.call(pizza).replace(/"/g,"")}
-            </ul>
+
+    getCardLi() {
+        let str = document.createElement('li');
+        str.innerHTML = `
+            <img src="./images/pizza_label.png" alt="pizza">
+            <span>${this.name}</span>
+            <span>${this.cost}</span>
+        `;
+        return str;
+    }
+
+    getCardGrid() {
+        let str = document.createElement('div');
+        str.classList.add('card');
+        str.id = this.name;
+        str.innerHTML = `
+        <div class="flip_effect_vertical hidden" id="wrap-image">
+            <img src=${this.getImage()} alt="pizza" class="front">
+            <img src="./images/pizza_label.png" alt="pizza" class="back">
         </div>
-    <p>Калории: ${pizza.calories}</p><p>Цена: ${pizza.cost}</p>`;
+        <div>
+            <p>${this.name.toUpperCase()}</p>
+            <label for="checkbox${this.name}" id="label_filling">Состав</label>
+            <div class="spoiler">
+                <input type="checkbox" class="hidden" id="checkbox${this.name}">
+                <div class="block">
+                    <ul>
+                        ${this.createFillingUl().replace(/"/g,"")}
+                    </ul>
+                    <input type="button" value="Добавить" onclick="Pizza.btnAddElement.call(this)">
+                    <input type="button" value="Удалить" onclick="btnDeleteElement.call(this)">
+                </div>
+            </div>
+            <p id="Calories">Калорий: ${this.calories}</p>
+            <p>Цена: ${this.cost}</p>
+        </div>`;
+        return str;
+    }
 
-    div.appendChild(img);
-    div.appendChild(name);
-    div.appendChild(label);
-    div.appendChild(str);
-    li.className= "ulGrid";
-    li.appendChild(div);
-    let ul = document.querySelector('#menu');
-    ul.appendChild(li);
+    getImage() {
+        if(this.imageURL.length != 0) return this.imageURL;
+        return "./images/default.jpg";
+    }
+
+    createFillingUl() {
+        let ul = document.createElement('ul');
+        let arr = this.filling.split(",");
+        arr.forEach(item =>{
+            let li = document.createElement('li');
+            li.innerHTML = item;
+            ul.appendChild(li);
+        });
+        return ul.innerHTML;
+    }
+
+    static btnAddElement () {
+        if(element.length == 0) return;
+        console.log(this.previousElementSibling);
+        let li = document.createElement('li');
+        li.innerHTML = element;
+        this.previousElementSibling.appendChild(li);
+    }
 }
-function createPizzaList(pizza) {
-    let div = document.createElement('div');
-    let li  = document.createElement('li');
-    let img = document.createElement('img');
-    let str = document.createElement("p")
 
-    img.src = "./images/pizza_label.png";
-    str.innerHTML = `${pizza.name.toUpperCase()}, Цена: ${pizza.cost}`;
+function btnDeleteElement () {
 
-    div.appendChild(img);
-    div.appendChild(str);
-    li.appendChild(div);
-    li.className ="ulList";
-    let ul = document.querySelector('#menu');
-    ul.appendChild(li);
 }
-function createMenuList() {
-    let ul = document.querySelector('#menu');
-    ul.innerHTML = "";
-    arrPizzas.forEach(pizza => {
-        createPizzaList(pizza);
+
+//------дополнительное меню для выбора ингридиента-------
+var element = "";
+function getClickElement () {
+    var list = document.getElementById('additionFillings');
+    list.addEventListener('click',(e)=>{
+        if(e.target.tagName == "P") {
+            element = e.target.innerHTML;
+            console.log(element);
+        }
     });
 }
-function createMenuGrid () {
-    let ul = document.querySelector('#menu');
-    ul.innerHTML = "";
-    this.forEach(pizza => {
-        createPizzaGrid(pizza);
+getClickElement();
+//------------------------------------------------------
+
+//-----------Фильтр по ингридиенту-----------------
+function getPizzasWithSpecifiedIngridient () {
+    var iText = document.getElementById('text');
+    let pattern = iText.value.toLowerCase();
+    store.forEach(pizza => {
+        if(pizza.filling.toLowerCase().indexOf(pattern)!=-1) {
+            pizza.matchesTheFilter = true;
+        } else {
+            pizza.matchesTheFilter = false;
+        }
     });
 }
-function sorter() {
+document.getElementById('text').addEventListener('change',getPizzasWithSpecifiedIngridient);
+document.getElementById('text').addEventListener('change',getMenu);
+//-------------------------------------------------
+
+//------------Сортировка------------------
+function sorter () {
 
     var selector = document.getElementById('select');
     var value = selector[selector.selectedIndex].value;
     switch (value) {
         case "priceUp":
-            arrPizzas.sort((a,b)=>a.cost - b.cost);
+            store.sort((a,b)=> a.cost - b.cost);
+            console.log(store);
             break;
         case "priceDown":
-            arrPizzas.sort((a,b)=>b.cost - a.cost);
+            store.sort((a,b)=>b.cost - a.cost);
+            console.log(store);
             break;
         case "A-Z":
-            arrPizzas.sort((a,b)=>{
+            store.sort((a,b)=>{
                 if(a.name>b.name) return 1;
                 if(a.name<b.name) return -1;
             });
+            console.log(store);
             break;
         case "Z-A":
-            arrPizzas.sort((a,b)=>{
+            store.sort((a,b)=>{
                 if(a.name>b.name) return 1;
                 if(a.name<b.name) return -1;
             }).reverse();
@@ -116,50 +145,122 @@ function sorter() {
     }
     console.log(value);
 }
-function filter() {
-    var iText = document.getElementById('text');
-    let patern = iText.value.toLowerCase();
-    let arr = arrPizzas.filter((pizza)=>{
-        if(pizza.filling.toLowerCase().indexOf(patern)!=-1) {
-            return true; 
-        } else {
-            return false;
+document.getElementById('select').addEventListener('change',sorter);
+document.getElementById('select').addEventListener('change',getMenu);
+//------------------------------------------
+
+
+//--------Функция отрисовки меню------------
+function getMenu () {
+    view = selector[selector.selectedIndex].value;
+    console.log(view);
+
+    if(store.length == 0) {
+        //создали экземпляры класса Pizza
+        arrPizzas.forEach( item => {
+            store.push( new Pizza(item) );
+        // container.appendChild(store[store.length-1].getCardGrid());
+        });
+    }
+    container.innerHTML = "";
+    menu.innerHTML = "";
+    store.forEach( pizza => {
+        if(pizza.matchesTheFilter) {
+
+            switch(view) {
+                case "grid" :
+                    btnFilling.classList.remove('hidden');
+                    divSort.classList.add('hidden');
+                    divIngridient.classList.remove('hidden');
+                    container.appendChild(pizza.getCardGrid());
+                    break;
+                case "list":
+                    btnFilling.classList.add('hidden');
+                    divIngridient.classList.add('hidden');
+                    divSort.classList.remove('hidden');
+                    menu.appendChild(pizza.getCardLi());
+                    break;
+            }
         }
     });
-    console.log(arr);
-    return arr;
 }
-function done() {
-    const newArr = filter();
-    createMenuGrid.call(newArr);
-}
+//---------------------------------------------
+document.getElementById('select-menu-view').addEventListener('change',getMenu);
 
 
-var view = confirm(`В каком режиме отображать страницу?  
-Ок - сетка; Отмена - список`);
 
-if(view) {
-    createMenuGrid.call(arrPizzas);
-    document.getElementById('select').classList.add('hidden');
-} else {
-    createMenuList();
-    document.getElementById('text').classList.add('hidden');
-}
+//-----------------
+dragElement(document.getElementById("additionFillings"));
 
-document.getElementById('select').addEventListener('change',sorter);
-document.getElementById('select').addEventListener('change',createMenuList);
-document.getElementById('text').addEventListener('change',done);
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
 
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+  }
 
-// let temp = document.getElementsByClassName('ulGrid');
-// let cards = [].slice.call(temp);
-// console.log(cards);
-// cards.forEach((card)=>{
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
     
-//     card.addEventListener("click",(e)=>{
-//         if(e.target.tagName!='IMG') {
-//             card.getElementsByTagName('img')[0].classList.remove('hidden');
-//         }
-//     });
-//     card.addEventListener('click', ()=> {card.classList.add('transformed')});
-// });
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+//--------------------
+
+// точка входа 
+var store = []; //массив в которм храняться все экземпляры класса Pizza
+
+let menu = document.getElementById('menu');
+let container = document.getElementById('container');
+var divSort = document.getElementById('div_sort');
+var divIngridient = document.getElementById('div_ingridient');
+var btnFilling = document.getElementById('btn_filling');
+var selector, flag, view;
+selector = document.getElementById('select-menu-view');
+flag = confirm(`В каком режиме отображать страницу?  
+Ок - сетка; Отмена - список`);
+if (flag) {
+        selector.options[0].selected = true;
+    } else{
+        selector.options[1].selected = true;
+    }
+getMenu();
+
+btnFilling.onclick = () => {
+    let ad = document.getElementById('additionFillings');
+    if(isOpen) { 
+        ad.classList.add('hidden');
+    } else {
+        ad.classList.remove('hidden');
+    }
+}
+var isOpen= false;
+store.forEach(pizza => {
+    document.getElementById(`${pizza.name}`).onclick = (e) => {
+        let image = e.currentTarget;
+        // image.classList.add('transformed');
+        image.querySelector('#wrap-image').classList.remove('hidden');
+    }
+});
+
